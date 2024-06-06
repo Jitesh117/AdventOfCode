7f8f2bfa769f36bb74bcae5135ed8122b3884871
jitesh117 <sahookumarjitesh@gmail.com>
2024-06-06 15:08:21 +0530

    [feat]: 2015 day 9

---
+++ 2015/day09.py
@@ -0,0 +1,25 @@
+import sys
+from itertools import permutations
+
+places = set()
+distances = dict()
+with open("input.txt") as file:
+    data = file.read()
+
+lines = data.splitlines()
+for line in lines:
+    (source, _, dest, _, distance) = line.split()
+    places.add(source)
+    places.add(dest)
+    distances.setdefault(source, dict())[dest] = int(distance)
+    distances.setdefault(dest, dict())[source] = int(distance)
+
+shortest = sys.maxsize
+longest = 0
+for items in permutations(places):
+    dist = sum(map(lambda x, y: distances[x][y], items[:-1], items[1:]))
+    shortest = min(shortest, dist)
+    longest = max(longest, dist)
+
+print("shortest: %d" % (shortest))
+print("longest: %d" % (longest))
